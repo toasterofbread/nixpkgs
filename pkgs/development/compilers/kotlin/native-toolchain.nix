ed11f12faf1c7298c10017777deb303b6204a011
Talo Halton <talohalton@gmail.com>
2024-06-15 23:13:21 +0100

    Update native-toolchain.nix

--- pkgs/development/compilers/kotlin/native-toolchain.nix
+++ pkgs/development/compilers/kotlin/native-toolchain.nix
@@ -1,24 +1,38 @@
 { lib
 , stdenv
 , fetchurl
-#, jre
-#, makeWrapper
+, jre
+, makeWrapper
 }:
 
 stdenv.mkDerivation rec {
-  pname = "kotlin-native-toolchain-x86";
+  pname = "kotlin-native-toolchain";
   version = "2.0.0";
 
   src = let
+    getArch = {
+      "aarch64-darwin" = "macos-aarch64";
+      "x86_64-darwin" = "macos-x86_64";
+      "x86_64-linux" = "linux-x86_64";
+    }.${stdenv.system} or (throw "${pname}-${version}: ${stdenv.system} is unsupported.");
+
+    getUrl = version: arch:
+      "https://download.jetbrains.com/kotlin/native/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2.tar.gz";
+
+    getHash = arch: {
+      "macos-aarch64" = "a048397d50fb5a2bd6cc0f89d5a30e0b8ff0373ebff9c1d78ce1f1fb7f185a50";
+      "macos-x86_64" = "a048397d50fb5a2bd6cc0f89d5a30e0b8ff0373ebff9c1d78ce1f1fb7f185a50";
+      "linux-x86_64" = "a048397d50fb5a2bd6cc0f89d5a30e0b8ff0373ebff9c1d78ce1f1fb7f185a50";
+    }.${arch};
   in
     fetchurl {
-      url = "https://download.jetbrains.com/kotlin/native/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2.tar.gz";
-      sha256 = "a048397d50fb5a2bd6cc0f89d5a30e0b8ff0373ebff9c1d78ce1f1fb7f185a50";
+      url = getUrl version getArch;
+      sha256 = getHash getArch;
     };
 
   nativeBuildInputs = [
-    #jre
-    #makeWrapper
+    jre
+    makeWrapper
   ];
 
   installPhase = ''
@@ -30,9 +44,9 @@ stdenv.mkDerivation rec {
     runHook postInstall
   '';
 
-  #postFixup = ''
-#    wrapProgram $out/bin/run_konan --prefix PATH ":" ${lib.makeBinPath [ jre ]}
-  #'';
+  postFixup = ''
+    wrapProgram $out/bin/run_konan --prefix PATH ":" ${lib.makeBinPath [ jre ]}
+  '';
 
   meta = {
     homepage = "https://kotlinlang.org/";
@@ -43,8 +57,8 @@ stdenv.mkDerivation rec {
       backend for the Kotlin compiler and native implementation of the Kotlin
       standard library.
     '';
-    #license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ ];
-    platforms = [ "x86_64-linux" ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ fabianhjr ];
+    platforms = [ "x86_64-linux" ] ++ lib.platforms.darwin;
   };
 }
