ccf22b26e86ac1816487051ffc53c52122fae711
toasterofbread <talohalton@gmail.com>
2024-06-16 00:54:53 +0100

    .

--- pkgs/development/compilers/kotlin/native-toolchain-x86.nix
+++ pkgs/development/compilers/kotlin/native-toolchain-x86.nix
@@ -10,31 +10,26 @@ stdenv.mkDerivation rec {
   
   src = "./";
   
-  fetchurl {
-    url = "https://download.jetbrains.com/kotlin/native/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2.tar.gz";
-    sha256 = "a048397d50fb5a2bd6cc0f89d5a30e0b8ff0373ebff9c1d78ce1f1fb7f185a50";
-  };
-
-  fetchurl {
-    url = "https://download.jetbrains.com/kotlin/native/lldb-4-linux.tar.gz";
-    sha256 = "b1e145c859f44071f66231cfc98c8c16a480cbf47139fcd5dd2df4bf041fdfda";
-  };
-
-  fetchurl {
-    url = "https://download.jetbrains.com/kotlin/native/llvm-11.1.0-linux-x64-essentials.tar.gz";
-    sha256 = "e5d8d31282f1eeefff006da74f763ca18ee399782d077ccd92693b51feb17a21";
-  };
-
-  fetchurl {
-    url = "https://download.jetbrains.com/kotlin/native/libffi-3.2.1-2-linux-x86-64.tar.gz";
-    sha256 = "9d817bbca098a2fa0f1d5a8b9e57674c30d100bb4c6aeceff18d8acc5b9f382c";
-  };
-
   srcs = [
-    "x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2"
-    "lldb-4-linux.tar.gz"
-    "llvm-11.1.0-linux-x64-essentials.tar.gz"
-    "libffi-3.2.1-2-linux-x86-64.tar.gz"
+    fetchurl {
+      url = "https://download.jetbrains.com/kotlin/native/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2.tar.gz";
+      sha256 = "a048397d50fb5a2bd6cc0f89d5a30e0b8ff0373ebff9c1d78ce1f1fb7f185a50";
+    }
+
+    fetchurl {
+      url = "https://download.jetbrains.com/kotlin/native/lldb-4-linux.tar.gz";
+      sha256 = "b1e145c859f44071f66231cfc98c8c16a480cbf47139fcd5dd2df4bf041fdfda";
+    }
+
+    fetchurl {
+      url = "https://download.jetbrains.com/kotlin/native/llvm-11.1.0-linux-x64-essentials.tar.gz";
+      sha256 = "e5d8d31282f1eeefff006da74f763ca18ee399782d077ccd92693b51feb17a21";
+    }
+
+    fetchurl {
+      url = "https://download.jetbrains.com/kotlin/native/libffi-3.2.1-2-linux-x86-64.tar.gz";
+      sha256 = "9d817bbca098a2fa0f1d5a8b9e57674c30d100bb4c6aeceff18d8acc5b9f382c";
+    }
   ];
   
   nativeBuildInputs = [ autoPatchelfHook ];
