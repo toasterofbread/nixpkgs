6fb4495288f611b0d30ed60c90df52e667dabfd9
toasterofbread <talohalton@gmail.com>
2024-06-16 02:05:13 +0100

    .

--- pkgs/development/compilers/kotlin/native-toolchain-x86.nix
+++ pkgs/development/compilers/kotlin/native-toolchain-x86.nix
@@ -8,22 +8,22 @@ stdenv.mkDerivation rec {
   version = "2.0.0";
   
   srcs = [
-    (builtin.fetchTarball {
+    (builtins.fetchTarball {
       url = "https://download.jetbrains.com/kotlin/native/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2.tar.gz";
       sha256 = "0922kif8z28yvvzpdh3bwf8i21dym66hxj47q0vrhh7nbynqbii2";
       name = "x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2";
     })
-    (builtin.fetchTarball {
+    (builtins.fetchTarball {
       url = "https://download.jetbrains.com/kotlin/native/lldb-4-linux.tar.gz";
       sha256 = "19r56d7h9zcdy0k6rksli1nvdwqxcpa0zy9akcxzxa6pba0ivw8x";
       name = "lldb-4-linux";
     })
-    (builtin.fetchTarball {
+    (builtins.fetchTarball {
       url = "https://download.jetbrains.com/kotlin/native/llvm-11.1.0-linux-x64-essentials.tar.gz";
       sha256 = "1yr476d4l3wm7ggid6z77bzs4qsis56spvq0ksav8ygd6p5zxh2z";
       name = "llvm-11.1.0-linux-x64-essentials";
     })
-    (builtin.fetchTarball {
+    (builtins.fetchTarball {
       url = "https://download.jetbrains.com/kotlin/native/libffi-3.2.1-2-linux-x86-64.tar.gz";
       sha256 = "0qp66pvka2mpnzcpg22bjcnlx7cpvircc4ha254xcclw5kvbw2fl";
       name = "libffi-3.2.1-2-linux-x86-64";
