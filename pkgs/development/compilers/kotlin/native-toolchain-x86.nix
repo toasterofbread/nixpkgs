75af3bd86233c5ade09678a837b1d22c30f7e98c
toasterofbread <talohalton@gmail.com>
2024-06-16 02:22:29 +0100

    .

--- pkgs/development/compilers/kotlin/native-toolchain-x86.nix
+++ pkgs/development/compilers/kotlin/native-toolchain-x86.nix
@@ -1,7 +1,6 @@
 { lib
 , stdenv
 , autoPatchelfHook
-, fetchurl
 }:
 
 stdenv.mkDerivation rec {
@@ -9,19 +8,19 @@ stdenv.mkDerivation rec {
   version = "2.0.0";
   
   srcs = [
-    (fetchurl {
+    (builtins.fetchTarball {
       url = "https://download.jetbrains.com/kotlin/native/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2.tar.gz";
       sha256 = "0922kif8z28yvvzpdh3bwf8i21dym66hxj47q0vrhh7nbynqbii2";
     })
-    (fetchurl {
+    (builtins.fetchTarball {
       url = "https://download.jetbrains.com/kotlin/native/lldb-4-linux.tar.gz";
       sha256 = "19r56d7h9zcdy0k6rksli1nvdwqxcpa0zy9akcxzxa6pba0ivw8x";
     })
-    (fetchurl {
+    (builtins.fetchTarball {
       url = "https://download.jetbrains.com/kotlin/native/llvm-11.1.0-linux-x64-essentials.tar.gz";
       sha256 = "1yr476d4l3wm7ggid6z77bzs4qsis56spvq0ksav8ygd6p5zxh2z";
     })
-    (fetchurl {
+    (builtins.fetchTarball {
       url = "https://download.jetbrains.com/kotlin/native/libffi-3.2.1-2-linux-x86-64.tar.gz";
       sha256 = "0qp66pvka2mpnzcpg22bjcnlx7cpvircc4ha254xcclw5kvbw2fl";
     })
