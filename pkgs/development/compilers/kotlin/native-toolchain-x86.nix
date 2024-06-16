90253d2e322bf7f87d7c0665d9ec4c7256a8ec9e
toasterofbread <talohalton@gmail.com>
2024-06-16 01:00:36 +0100

    .

--- pkgs/development/compilers/kotlin/native-toolchain-x86.nix
+++ pkgs/development/compilers/kotlin/native-toolchain-x86.nix
@@ -10,41 +10,46 @@ stdenv.mkDerivation rec {
   
   src = "./";
   
-  srcs = [
-    fetchurl {
-      url = "https://download.jetbrains.com/kotlin/native/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2.tar.gz";
-      sha256 = "a048397d50fb5a2bd6cc0f89d5a30e0b8ff0373ebff9c1d78ce1f1fb7f185a50";
-    }
-
-    fetchurl {
-      url = "https://download.jetbrains.com/kotlin/native/lldb-4-linux.tar.gz";
-      sha256 = "b1e145c859f44071f66231cfc98c8c16a480cbf47139fcd5dd2df4bf041fdfda";
-    }
-
-    fetchurl {
-      url = "https://download.jetbrains.com/kotlin/native/llvm-11.1.0-linux-x64-essentials.tar.gz";
-      sha256 = "e5d8d31282f1eeefff006da74f763ca18ee399782d077ccd92693b51feb17a21";
-    }
-
-    fetchurl {
-      url = "https://download.jetbrains.com/kotlin/native/libffi-3.2.1-2-linux-x86-64.tar.gz";
-      sha256 = "9d817bbca098a2fa0f1d5a8b9e57674c30d100bb4c6aeceff18d8acc5b9f382c";
-    }
-  ];
-  
   nativeBuildInputs = [ autoPatchelfHook ];
 
-  installPhase = ''
-    runHook preInstall
+  installPhase = let
+    gcc = fetchurl {
+      url = "https://download.jetbrains.com/kotlin/native/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2.tar.gz";
+      sha256 = "a048397d50fb5a2bd6cc0f89d5a30e0b8ff0373ebff9c1d78ce1f1fb7f185a50";
+    };
 
-    for s in $srcs
-    do
-      mkdir -p $out/$s
-      mv $s/* $out/$s
-    done
+    lldb = fetchurl {
+      url = "https://download.jetbrains.com/kotlin/native/lldb-4-linux.tar.gz";
+      sha256 = "b1e145c859f44071f66231cfc98c8c16a480cbf47139fcd5dd2df4bf041fdfda";
+    };
 
-    runHook postInstall
-  '';
+    llvm = fetchurl {
+      url = "https://download.jetbrains.com/kotlin/native/llvm-11.1.0-linux-x64-essentials.tar.gz";
+      sha256 = "e5d8d31282f1eeefff006da74f763ca18ee399782d077ccd92693b51feb17a21";
+    };
+
+    libffi = fetchurl {
+      url = "https://download.jetbrains.com/kotlin/native/libffi-3.2.1-2-linux-x86-64.tar.gz";
+      sha256 = "9d817bbca098a2fa0f1d5a8b9e57674c30d100bb4c6aeceff18d8acc5b9f382c";
+    };
+  in
+    ''
+      runHook preInstall
+
+      mkdir -p $out/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2
+      mv $gcc/* $out/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2
+
+      mkdir -p $out/lldb-4-linux.tar.gz
+      mv $lldb/* $out/lldb-4-linux.tar.gz
+
+      mkdir -p $out/llvm-11.1.0-linux-x64-essentials.tar.gz
+      mv $llvm/* $out/llvm-11.1.0-linux-x64-essentials.tar.gz
+
+      mkdir -p $out/libffi-3.2.1-2-linux-x86-64.tar.gz
+      mv $libffi/* $out/libffi-3.2.1-2-linux-x86-64.tar.gz
+
+      runHook postInstall
+    '';
 
   meta = {
     homepage = "https://kotlinlang.org/";
